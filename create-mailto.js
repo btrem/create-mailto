46ea85e8aaba7a49e565f498e1d253acb075362a
Brian Tremblay <dev@btrem.com>
2020-11-22 14:01:16 -0800

    Refactors element excludes
    Changes forbidden element array to string. Changes
    first exclude from tag comparison to Node.matches.
    Renames const names for code clarity.

--- create-mailto.js
+++ create-mailto.js
@@ -1,7 +1,7 @@
 /*
  * create-mailto.js
  *
- * version 0.5
+ * version 0.51
  *
  * find email addresses in web pages and turn them into clickable <a href=mailto>
  */
@@ -10,42 +10,37 @@
 
 var elementCollection, emailAddress, mailtoElement;
 
-// elements that can't be made into, nor be ancestors of, nor be inside anchor links
-const forbiddenElements = [
-    'a', 'audio', 'button', 'details', 'input', 'label', 'meter', 'progress', 'select', 'textarea', 'video'
-];
+// interactive elements that have to be handled specially
+const interactiveElements = 'a, audio, button, details, input, label, meter, progress, select, textarea, video';
 
-// elements that can't be anchor links, nor be inside anchors (as above),
-// but can be ancestors (e.g., a form can have a link inside)
-let forbiddenElementsPlus = forbiddenElements;
-forbiddenElementsPlus.push('fieldset', 'form', 'iframe', 'math');
-
-// TODO improve variable names for forbidden elements
+// container elements that have to handled specially
+const containerElements = 'fieldset, form, iframe, math';
 
 window.addEventListener('DOMContentLoaded', () => {
 
     // get elements inside  <body> whose class attribute includes 'email', 'u-email', or 'p-email';
 	// or whose itemprop attribute includes 'email'; or with a data-email attribute
 
-    elementCollection = document.querySelector('body');
+    elementCollection = document
+        .querySelector('body')
+        .querySelectorAll('.email, .u-email, .p-email, [itemprop~=email], [data-email]');
 
-    elementCollection = elementCollection.querySelectorAll('.email, .u-email, .p-email, [itemprop~=email], [data-email]');
+    elementCollection.forEach(function(el) {
 
-    elementCollection.forEach(function(el){
-
-	    // exclude elements that are can't be/contain/be inside of anchor links
-
-        if ( forbiddenElementsPlus.includes( el.tagName.toLowerCase() ) ) {
+        // exclude element if it can't be anchor link
+        if ( el.matches(interactiveElements + ',' + containerElements) ) {
             return;
         }
-        if ( el.querySelectorAll( forbiddenElementsPlus.join() ).length ) {
+	    // exclude element if it has problem descendents
+        if ( el.querySelector(interactiveElements + ',' + containerElements)  ) {
             return;
         }
-        if ( el.closest( forbiddenElements.join() ) ) {
+	    // exclude element if it has problem ancestors
+        if ( el.closest(interactiveElements) ) {
             return;
         }
 
-        // get email address, preferring data-email attribute when it's available, text content if not
+        // get email address from data-email attribute (preferred) or text content
 		emailAddress = el.dataset.email || el.innerText;
 
 		emailAddress = emailAddress.trim();
