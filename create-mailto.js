205a5818f61517e02eb6399fc71e54f60c72f798
Brian Tremblay <dev@btrem.com>
2020-11-21 18:48:28 -0800

    Adds elements that shouldn’t be made into email links
    Prevents script from making other elements besides
    `<a>` into mailto: links, including interactive
    and form elements.
    
    TODO: improve variable names

--- create-mailto.js
+++ create-mailto.js
@@ -1,7 +1,7 @@
 /*
  * create-mailto.js
  *
- * version 0.4
+ * version 0.5
  *
  * find email addresses in web pages and turn them into clickable <a href=mailto>
  */
@@ -9,6 +9,18 @@
 'use strict'
 
 var elementCollection, emailAddress, mailtoElement;
+
+// elements that can't be made into, nor be ancestors of, nor be inside anchor links
+const forbiddenElements = [
+    'a', 'audio', 'button', 'details', 'input', 'label', 'meter', 'progress', 'select', 'textarea', 'video'
+];
+
+// elements that can't be anchor links, nor be inside anchors (as above),
+// but can be ancestors (e.g., a form can have a link inside)
+let forbiddenElementsPlus = forbiddenElements;
+forbiddenElementsPlus.push('fieldset', 'form', 'iframe', 'math');
+
+// TODO improve variable names for forbidden elements
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -21,9 +33,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
     elementCollection.forEach(function(el){
 
-	    // exclude elements that are <a>, a descendent of <a>, or contain <a>
+	    // exclude elements that are can't be/contain/be inside of anchor links
 
-        if ( el.tagName.toLowerCase() === "a" || el.getElementsByTagName('a').length || el.closest('a') ) {
+        if ( forbiddenElementsPlus.includes( el.tagName.toLowerCase() ) ) {
+            return;
+        }
+        if ( el.querySelectorAll( forbiddenElementsPlus.join() ).length ) {
+            return;
+        }
+        if ( el.closest( forbiddenElements.join() ) ) {
             return;
         }
 
