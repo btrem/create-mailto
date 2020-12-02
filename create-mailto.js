88929f9a1445500462b54105f5a9eb2659ef265d
Brian Tremblay <dev@btrem.com>
2020-12-01 18:45:42 -0800

    Changes main forEach loop to use filtered array
    Converts element collection to array. Filters
    elements out of array if they should not be
    converted to mailto, then starts main forEach
    loop.

--- create-mailto.js
+++ create-mailto.js
@@ -1,48 +1,64 @@
 /*
  * create-mailto.js
  *
- * version 0.6
+ * version 0.7
  *
  * find email addresses in web pages and turn them into clickable <a href=mailto>
  */
 
 'use strict'
 
-var elementCollection, emailAddress, mailtoElement;
+var elementCollection, elementArray, elementArrayFiltered, emailAddress, mailtoElement;
 
-// elements that have to be handled specially
-const specialElements = 'a, audio, br, button, canvas, col, details, embed, ' +
+// query selector for mailto elements: class attribute includes 'email',
+// 'u-email', or 'p-email'; or itemprop attribute includes 'email';
+// or has data-email attribute
+const mailtoSelector = '.email, .u-email, .p-email, [itemprop~=email], [data-email]';
+
+// query selector for elements that can't be links, nor be inside links, nor contain links
+const excludeSelector = 'a, audio, br, button, canvas, col, details, embed, ' +
                         'hr, input, label, map, math, meter, object, param, ' +
                         'progress, select, source, textarea, video';
 
-// container elements that have to handled specially
-const containerElements = 'fieldset, form, iframe';
+// query selector for elements that can't be links nor be inside links, but can contain links
+const excludeSelectorUnlessContainer = 'fieldset, form, iframe';
 
+// some elements can't contain links (<img> is void, and <picture> can't contain <a>); they must be wrapped
+const outerWrapElements = 'img, picture';
+
+// class to allow authors to exclude elements from create-mailto processing
 const excludeClass = '.exclude-create-mailto';
+
+function canBeMailto(elmt) {
+
+    if ( elmt.matches(excludeSelector + ',' + excludeSelectorUnlessContainer + ',' + excludeClass ) ) {
+        return false;
+    }
+    // exclude element if it has descendent that a link can't contain
+    if ( elmt.querySelector(excludeSelector + ',' + excludeSelectorUnlessContainer)  ) {
+        return false;
+    }
+    // exclude element if it has ancestor that can't contain links
+    if ( elmt.closest(excludeSelector) ) {
+        return false;
+    }
+
+    return true;
+}
+
 
 window.addEventListener('DOMContentLoaded', () => {
 
     // get elements inside <body> whose class attribute includes 'email', 'u-email', or 'p-email';
 	// or whose itemprop attribute includes 'email'; or with a data-email attribute
 
-    elementCollection = document
-        .getElementsByTagName('body')[0]
-        .querySelectorAll('.email, .u-email, .p-email, [itemprop~=email], [data-email]');
+    elementCollection = document.getElementsByTagName('body')[0].querySelectorAll(mailtoSelector);
 
-    elementCollection.forEach(function(el) {
+    elementArray = Array.from(elementCollection);
 
-        // exclude problem elements and elements with special exclude class
-        if ( el.matches(specialElements + ',' + containerElements + ',' + excludeClass) ) {
-            return;
-        }
-	    // exclude element if it has problem descendents
-        if ( el.querySelector(specialElements + ',' + containerElements)  ) {
-            return;
-        }
-	    // exclude element if it has problem ancestors
-        if ( el.closest(specialElements) ) {
-            return;
-        }
+    elementArrayFiltered = elementArray.filter(canBeMailto);
+
+    elementArrayFiltered.forEach(function(el) {
 
         // get email address from data-email attribute (preferred) or text content
 		emailAddress = el.dataset.email || el.innerText;
@@ -56,19 +72,19 @@ window.addEventListener('DOMContentLoaded', () => {
 			mailtoElement = document.createElement('a');
             mailtoElement.setAttribute('href', 'mailto:' + emailAddress);
 
-			// if target element is <img>, it is void; move <img> inside <a>
-			if (el.tagName.toLowerCase() == 'img') {
-                // insert new empty <a> element into dom after target img element
+            // check if element can't contain link
+            if ( el.matches(outerWrapElements ) ) {
+                // insert new empty <a> element into dom after target element
                 el.insertAdjacentElement('afterend', mailtoElement);
-                // move target img element inside <a>
+                // move target element inside inserted <a>
                 mailtoElement.append(el);
 			}
             else {
-                // move contents of target element in DOM to inside of mailtoElement which is not yet in DOM
+                // move children of target element out of DOM and into mailtoElement
                 while (el.firstChild) {
                     mailtoElement.appendChild(el.firstChild);
                 }
-			    // append mailtoElement to target element, which places it in DOM
+			    // append mailtoElement to target element, putting it back into DOM
                 el.append(mailtoElement);
             }
 
