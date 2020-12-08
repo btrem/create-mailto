f9f27b3fb50915c62e9adefc781a3749f7c0eb39
Brian Tremblay <dev@btrem.com>
2020-12-08 12:57:20 -0800

    Changes error outputs
    Adds class name to test results container
    paragraph. Renames output class names. Moves those
    class attributes from label to output elements.
    Adds output to show number of document fragments
    tested. Shortens pass fail result label

--- tests/test-nope.js
+++ tests/test-nope.js
@@ -4,14 +4,18 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     // output elements show test results
-    const errorsOutput = document.querySelector('.errors > output');
-    const resultOutput = document.querySelector('.result > output');
+    const results = document.querySelector('p.results');
+    const testsCount = document.querySelector('output.testsCount');
+    const errorsCount = document.querySelector('output.errorsCount');
+    const passFail = document.querySelector('output.passFail');
 
+    const rows = document.querySelectorAll('tbody > tr');
     // there should be no mailto: links anywhere in table body
     const errors = document.querySelectorAll('tbody a[href^="mailto:"]').length;
 
-    errorsOutput.value = errors;
-    resultOutput.value = (errors) ? 'failed' : 'passed';
-    resultOutput.parentElement.parentElement.className = (errors) ? 'failed' : 'passed';
+    testsCount.value = rows.length;
+    errorsCount.value = errors;
+    passFail.value = (errors) ? 'failed' : 'passed';
+    results.className = (errors) ? 'failed' : 'passed';
 
 }); // end wrapper DOMContentLoaded function
--- tests/test-yep.js
+++ tests/test-yep.js
@@ -4,8 +4,10 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     // output elements show test results
-    const errorsOutput = document.querySelector('.errors > output');
-    const resultOutput = document.querySelector('.result > output');
+    const results = document.querySelector('p.results');
+    const testsCount = document.querySelector('output.testsCount');
+    const errorsCount = document.querySelector('output.errorsCount');
+    const passFail = document.querySelector('output.passFail');
 
     let errors = 0;
     // there should be one mailto: link for each row in the table body
@@ -16,8 +18,9 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    errorsOutput.value = errors;
-    resultOutput.value = (errors) ? 'failed' : 'passed';
-    resultOutput.parentElement.parentElement.className = (errors) ? 'failed' : 'passed';
+    testsCount.value = rows.length;
+    errorsCount.value = errors;
+    passFail.value = (errors) ? 'failed' : 'passed';
+    results.className = (errors) ? 'failed' : 'passed';
 
 }); // end wrapper DOMContentLoaded function
