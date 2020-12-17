8ea1d55b6918d0d471690a976bb90402683047c0
Brian Tremblay <dev@btrem.com>
2020-12-17 14:47:48 -0800

    Combines test-yep.js and test-nope.js into one script

--- tests/test.js
+++ tests/test.js
@@ -9,16 +9,25 @@ window.addEventListener('DOMContentLoaded', () => {
     const errorsCount = document.querySelector('output.errorsCount');
     const passFail = document.querySelector('output.passFail');
 
+    // initialize errors
     let errors = 0;
-    // there should be one mailto: link for each row in the table body
-    const rows = document.querySelectorAll('tbody > tr');
+
+    // number of tests is number of rows in table body
+    testsCount.value = document.querySelectorAll('tbody > tr').length;
+
+    // in table body with class=mailtoYep, there should be one mailto: link in each row;
+    // get NodeList of rows in tbody class=mailtoYep
+    const rows = document.querySelectorAll('tbody.mailtoYep > tr');
+    // convert NodeList to array and search each row for mailto:
     Array.from(rows).forEach(function(row) {
         if (row.querySelectorAll('a[href^="mailto:"]').length !== 1) {
             errors += 1;
         }
     });
 
-    testsCount.value = rows.length;
+    // in table body with class=mailtoNope, there should be no mailto: links
+    errors += document.querySelectorAll('tbody.mailtoNope a[href^="mailto:"]').length;
+
     errorsCount.value = errors;
     passFail.value = (errors) ? 'failed' : 'passed';
     results.className = (errors) ? 'failed' : 'passed';
